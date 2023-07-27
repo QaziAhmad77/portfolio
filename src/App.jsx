3649c025868706dcc2fbfdae5e7093f56bbd56b4
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-07-28 03:20:35 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Projects from './components/Projects';
+import Banner from './components/banner/Banner';
+import Navbar from './components/navbar/Navbar';
+import { Features } from './components/features/Features';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className=" text-4xl font-bold text-red-800">Qazi Ahmad</h1>
+    <div className="w-full bg-bodyColor h-auto text-lightText">
+      <div className="max-w-screen-2xl  mx-auto px-16">
+        <Navbar />
+        <Banner />
+        <Features />
+        <Projects />
+      </div>
     </div>
   );
 };
