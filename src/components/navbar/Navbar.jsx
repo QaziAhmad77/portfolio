153bed53a19fb28380069134cfedf0af563ba0ec
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-07-30 01:53:18 +0500

    Create Navbar.jsx

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-scroll';
+import { logo } from '../../assets/index';
+import { navLinksdata } from '../../constants';
+
+const Navbar = () => {
+  return (
+    <div className=" w-full h-24 mx-auto  flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600 sticky top-0 z-50 bg-bodyColor">
+      <div>
+        <img className="w-[70px] h-[70px] rounded-full" src={logo} alt="" />
+      </div>
+      <div>
+        <ul className="flex items-center gap-10">
+          {navLinksdata.map(({ _id, title, link }) => (
+            <li
+              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+              key={_id}
+            >
+              <Link
+                activeClass="active"
+                to={link}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
