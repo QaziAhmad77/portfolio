d1e67ba53488207ee337473e533d805c45b2accf
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-08-02 02:22:47 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Banner from './components/banner/Banner';
 import Navbar from './components/navbar/Navbar';
 import { Features } from './components/features/Features';
 import Resume from './components/resume/Resume';
+import Testimonial from './components/testimonial/Testimonial';
 
 const App = () => {
   return (
-    <div className="w-full bg-bodyColor h-auto text-lightText">
+    <div className="w-full bg-bodyColor h-auto text-lightText p-4">
+      <Navbar />
       <div className="max-w-screen-2xl  mx-auto px-16">
-        <Navbar />
         <Banner />
         <Features />
         <Projects />
         <Resume />
+        <Testimonial />
       </div>
     </div>
   );
