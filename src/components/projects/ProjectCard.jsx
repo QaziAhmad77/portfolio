7a40d82cfd84d0384f8a7bc177755e5feca4d7f5
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-08-11 01:15:17 +0500

    updated

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import { BsGithub } from 'react-icons/bs';
 import { FaGlobe } from 'react-icons/fa';
-const ProjectCard = ({ title, des, src, link }) => {
+const ProjectCard = ({ title, des, src, link,url }) => {
   return (
     <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
       <div className="w-full h-[80%] overflow-hidden rounded-lg">
@@ -23,7 +23,9 @@ const ProjectCard = ({ title, des, src, link }) => {
                 </a>
               </span>
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-                <FaGlobe />
+               <a href={url}>
+               <FaGlobe />
+               </a>
               </span>
             </div>
           </div>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -20,30 +20,35 @@ const Projects = () => {
           des="The Doctor Appointment System is built using the MERN technology stack,The app is a sophisticated and user-friendly application designed to streamline and enhance the process of scheduling medical appointments. "
           src={doctor}
           link="https://github.com/QaziAhmad77/Doctor-Appointment-system"
+          url="https://rtk-ecommerce-ten.vercel.app/"
         />
         <ProjectCard
           title="Shopping Cart"
           des='The "Shopping Cart" project is a user-friendly web application designed to provide a seamless and engaging online shopping experience. This application utilizes React.js, Bootstrap, HTML, and CSS to create an visually appealing platform for users '
           src={cart}
           link="https://github.com/QaziAhmad77/redux-cart"
+          url="https://rtk-ecommerce-ten.vercel.app/"
         />
         <ProjectCard
           title="Movie App"
           des="The 'Movie App' is a captivating and feature-rich web application that caters to movie enthusiasts by providing them with a user-friendly platform. With a powerful technology stack that includes React.js with Redux, SASS, HTML, and integration with an external API for fetching movie data, "
           src={movie}
           link="https://github.com/QaziAhmad77/RTK-movies-site"
+          url="https://rtk-movies-site.vercel.app/"
         />
         <ProjectCard
           title="Real Estate Website"
           des="The 'Real Estate Website' is a visually appealing and user-centric web application designed to provide users with a seamless experience for exploring and discovering real estate properties. This project utilizes the Tailwind CSS framework and HTML to create a modern and responsive platform for users"
           src={realEstate}
           link="https://github.com/QaziAhmad77/tailwindCSS/tree/main/realEstateWebsite"
+          url="https://tailwind-css-snowy.vercel.app/"
         />
         <ProjectCard
           title="Advance ToDo App"
           des="The 'Advanced ToDo App' is a feature-rich and interactive web application that takes task management to the next level. Built with React, Redux, and React Persist, this app offers users a powerful tool to organize their tasks, ensuring a seamless experience even across different sessions."
           src={todo}
           link="https://github.com/QaziAhmad77/toDoList"
+          url="https://to-do-list-nine-woad.vercel.app/"
         />
       </div>
     </section>
